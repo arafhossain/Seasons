c87adc1a432de13b9060b786f9cd6dd83b61aa2c
Araf Hossain <arafh13@gmail.com>
2019-08-06 16:53:04 -0400

    Converted into Class based component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition((position) => { console.log(position) }, (error) => { console.log(error) });
-
-    return (
-        <div>Howdy!</div>
-    )
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { console.log(position) },
+            (error) => { console.log(error) }
+        );
+        <div>Latitude: </div>
+    }
 }
 
 ReactDOM.render(
