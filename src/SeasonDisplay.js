c75abc6f417d0359ec9733f796625440ce71b59a
Araf Hossain <arafh13@gmail.com>
2019-08-26 15:54:10 -0400

    Refactored to conditional rendering

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,8 +1,26 @@
 import React from 'react';
 
-const SeasonDisplay = () =>{
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9){
+        if(lat > 0){
+            return 'Summer';
+        } else {
+            return 'Winter';
+        }
+    } else {
+        if(lat > 0){
+            return 'Winter';
+        } else {
+            return 'Summer';
+        }
+    }
+}
+
+const SeasonDisplay = (props) =>{
+    const Season = getSeason(props.lat, new Date().getMonth());
+    let phrase = Season === 'winter' ? 'Yikes, too cold!' : 'To the beach!'
     return (
-        <div>Season</div>
+        <h1>{phrase}</h1>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
     state = {lat: null, errorMessage: ''};
@@ -10,13 +11,14 @@ class App extends React.Component {
         );
     }
     render() {
-        return (
-            <div>
-                Latitude: {this.state.lat}
-                <br />
-                Error: {this.state.errorMessage}
-            </div>
-        )
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+        return <div>Loading!</div>;
     }
 }
 
